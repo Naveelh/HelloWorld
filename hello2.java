0736e1963fad4dba0a6e1ce02bdde61557cc4736
NAVEEL HASAN <42932445+Naveelh@users.noreply.github.com>
2019-07-25 23:48:04 +0530

    Update hello2.java

--- hello2.java
+++ hello2.java
@@ -3,7 +3,7 @@ package lab;
 public class hello2 {
 	
 	public static void main(String[] args){
-		System.out.println("Hello World i am testing doing changes againnnnnnnnnnn");
+		System.out.println("Hello World i am testing doing changes againnnnnnnnnnnnnnnnnnnnnn");
 	}
 
 }
